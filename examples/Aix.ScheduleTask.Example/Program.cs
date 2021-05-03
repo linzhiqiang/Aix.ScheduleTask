a31e3046a2232170695a054701a4e6d920bfb86a
linzhiqiang <360779475@qq.com>
2021-05-03 12:10:27 +0800

    test

--- examples/Aix.ScheduleTask.Example/Program.cs
+++ examples/Aix.ScheduleTask.Example/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading;
 
 namespace Aix.ScheduleTask.Example
 {
@@ -7,6 +9,7 @@ namespace Aix.ScheduleTask.Example
     {
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(200, 200);
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -18,7 +21,13 @@ namespace Aix.ScheduleTask.Example
                })
               .ConfigureAppConfiguration((hostBulderContext, configurationBuilder) =>
               {
-
+                  //配置环境变量 ASPNETCORE _ENVIRONMENT: Development/Staging/Production(默认值) 
+                  //以下加载配置文件的方式，是系统的默认行为，如果改变配置文件路径 需要自己加载，否则没必要了
+                  /*
+                   var environmentName = hostBulderContext.HostingEnvironment.EnvironmentName;
+                   configurationBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                   configurationBuilder.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);// 覆盖前面的相同内容
+                 */
               })
                .ConfigureLogging((hostBulderContext, loggingBuilder) =>
                {
